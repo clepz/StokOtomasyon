e17352562a038a17c88d4aa8abd9ba502649d57d
Tarik Umutlu <tarik.umutlu@outlook.com>
2018-10-23 18:34:40 +0300

    commit1

---
+++ src/main/java/com/tarikaskin/stok/StokApplication.java
@@ -0,0 +1,12 @@
+package com.tarikaskin.stok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StokApplication.class, args);
+    }
+}
